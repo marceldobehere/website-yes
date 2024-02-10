9dd83cd19eced650d4bf93e1c4219207fb4211c4
Masl <marcel.z2006@gmail.com>
2024-02-11 00:11:45 +0100

    footer

---
+++ src/app/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = ({page, title}) => {
+    return (
+        <>
+            <div className="footer-div">
+                <p>© 2024 Marcel Zietek</p>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
